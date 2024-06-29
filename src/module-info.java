493437db0184c25c40fc62f9b85aa375765da6b7
Marcel Ludwig <london_marcel@hotmail.com>
2024-06-28 21:49:47 -0300

    DepartmentService

--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,7 @@ module workshop {
 	requires javafx.controls;
 	requires javafx.fxml;
 	
+	opens model.entities to javafx.base;
 	opens gui to javafx.fxml;
 	opens application to javafx.graphics, javafx.fxml;
 }
